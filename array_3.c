3894a021f969317065652d667306815b7d95cece
hariom2882 <hariombansal2882@gmail.com>
2025-05-09 15:12:33 +0530

    Add files via upload

--- array_3.c
+++ array_3.c
@@ -1,27 +1,30 @@
-#include<stdio.h>
-void main(){
-    int t=5;
-    int count=0;
-    int count1=0;
-    int count2=0;
-    int arr[]={0,1,0,2,2};
-    for (int i = 0; i <=4; i++)
-    {
-        // printf("%d\n",arr[i]);
-    if (0==arr[i])
-    {
-       count++;
+#include <stdio.h>
+
+void main() {
+    int count = 0;
+    int count1 = 0;
+    int count2 = 0;
+    int arr[] = {0, 1, 0, 2, 2};
+
+    for (int i = 0; i <= 4; i++) {
+        if (arr[i] == 0) {
+            count++;
+        }
     }
-    if (1==arr[i])
-    {
-       count1++;
+
+    for (int j = 0; j <= 4; j++) {
+        if (arr[j] == 1) {
+            count1++;
+        }
     }
-    if (2==arr[i])
-    {
-       count2++;
-    }        
+
+    for (int a = 0; a <= 4; a++) {
+        if (arr[a] == 2) {
+            count2++;
+        }
     }
-    printf("no.0=%d\n",count);
-    printf("no.1=%d\n",count1);
-    printf("no.2=%d",count2);
+
+    printf("no.0 = %d\n", count);
+    printf("no.1 = %d\n", count1);
+    printf("no.2 = %d\n", count2);
 }
